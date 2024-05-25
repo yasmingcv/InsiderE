0b11f760e7b5be485cd816d67065f09cdfa43762
Murillo Oliveira <murillo1047@gmail.com>
2024-05-24 22:01:39 -0300

    feat: imagens dos pilotos e nomes deles na pagina home

--- js/script.js
+++ js/script.js
@@ -11,12 +11,29 @@ window.onload = function() {
                     // Atualize o DOM com as informações do time
                     const teamDetailsContainerText = document.getElementById('team-details-text');
                     const teamDetailsContainerImages = document.getElementById('team-details-images');
+                    const piloto1Container = document.getElementById('piloto1');
+                    const piloto2Container = document.getElementById('piloto2');
                     console.log(teamDetailsContainerImages);
                     teamDetailsContainerText.innerHTML = `
-                        <img src=${team.logo} alt="Logo do time">
+                        <div style="background-image: url('${team.logo}'); height: 180px; width: 300px; background-size: contain; background-repeat: no-repeat;">
+                        </div>
                         <p>${team.description}</p>
                     `;
                     teamDetailsContainerImages.style.background = `${team.background}`;
+                    piloto1Container.innerHTML = `
+                        <div>
+                            <h3 class="pilotoNome">${team.piloto1Nome}</h3>
+                            <h1 class="pilotoSobrenome">${team.piloto1Sobrenome}</h1>
+                        </div>
+                        <img class="imagem-piloto" src=${team.piloto1} alt="Logo do time">
+                    `;
+                    piloto2Container.innerHTML = `
+                            <div>
+                            <h3 class="pilotoNome">${team.piloto2Nome}</h3>
+                            <h1 class="pilotoSobrenome">${team.piloto2Sobrenome}</h1>
+                        </div>
+                        <img class="imagem-piloto" src=${team.piloto2} alt="Logo do time">
+                    `;
                 }
             })
             .catch(error => console.error('Error fetching the JSON:', error));
